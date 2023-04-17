8e0ce1c349963550fd93eafec19d1c11d2fc00eb
Timour <rurichek@hotmail.com>
2023-04-17 09:13:10 -0400

    second commit

--- autocomplete_starter_code/starter_code/script.js
+++ autocomplete_starter_code/starter_code/script.js
@@ -1,4 +1,4 @@
-const input = document.querySelector('#fruit'); //type our keyword here
+const inputBox = document.querySelector('#fruit'); //type our keyword here
 const suggestions = document.querySelector('.suggestions ul'); //display suggested keywords here
 
 const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
@@ -13,9 +13,9 @@ function search(str) {
 			fruit.toLowerCase().includes(input.toLowerCase());
 				return fruit.toLowerCase().includes(input.toLowerCase());
 		});
-		console.log(results)
+		// console.log(results)
 	}
-	console.log(results)
+	showSuggestions(results)
 	return results;
 	
 }
@@ -25,16 +25,26 @@ function searchHandler(e) {
 	
 }
 
-function showSuggestions(results, inputVal) {
+function showSuggestions(results) { //removed inputVal
+	const content = results.map((list) => {
+		return "<li>" + list + "</li>";
+	});
 
+	suggestions.innerHTML = content.join('')
 	// TODO
 }
 
-function useSuggestion(e) {
+function useSuggestion(event) {
 	// TODO
+	let list = event.target;
+
+	console.log(event.target);
+
+	inputBox.value = list.innerHTML;
+	suggestions.innerHTML = ''; 
 }
 
-input.addEventListener('keyup', searchHandler);
+inputBox.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
 // Step five and six; create an event which triggers search function when a user types
-input.addEventListener('keyup', search);
+inputBox.addEventListener('keyup', search);
